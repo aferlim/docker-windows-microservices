a553ab19d530156c16cd6a043ff12f1302d413a6
André Ferreira Lima <andre.lima@ltm.digital>
2018-08-13 00:19:49 -0300

    first commit

---
+++ standard-api/Controllers/SettingsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace standard_api.Controllers
+{
+    [Route("api/settings")]
+    public class SettingsController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] {
+                ConfigurationManager.AppSettings["setting-test"],
+                ConfigurationManager.ConnectionStrings["connection-test"].ConnectionString,
+                "value2" };
+        }
+    }
+}
---
+++ standard-api/Infrastructure/Domain/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using standard_api.Infrastructure.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace standard_api.Infrastructure.Domain
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("name=SchoolDBConnectionString")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
